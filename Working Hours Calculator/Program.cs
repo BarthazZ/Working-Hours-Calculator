af01f9fbca9d3d16997a78798216174125beb525
Bartłomiej Martenka <94762027+BarthazZ@users.noreply.github.com>
2022-01-26 14:48:10 +0100

    Dodaj pliki projektów.

---
+++ Working Hours Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Working_Hours_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the start time.");
+            var starth = DateTime.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Enter the end time.");
+            var endh = DateTime.Parse(Console.ReadLine());
+            
+            var timespan = endh - starth;
+            Console.WriteLine("I was working  " + timespan + "  hh:mm:ss.");
+        }
+    }
+}
